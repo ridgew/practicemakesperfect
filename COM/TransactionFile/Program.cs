2b7b3f61db308780efd87d45212508aacc8f97f9
Ridge W <email@qinjun.wang>
2019-05-17 19:58:21 +0800

    TXF sample

---
+++ COM/TransactionFile/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Transactions;
+
+namespace KtmIntegration
+{
+
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (TransactionScope transcope = new TransactionScope())
+                {
+                    Console.WriteLine("输入要删除的文件");
+                    string path = Console.ReadLine();
+                    Microsoft.KtmIntegration.TransactedFile.Delete(path);
+                    Console.WriteLine("是否提交事务处理？");
+                    if (Console.ReadLine() == "y")
+                        transcope.Complete();
+                    else
+                        Transaction.Current.Rollback();
+                }
+            }
+            catch (Exception err) { Console.WriteLine(err); }
+        }
+    }
+}
---
+++ COM/TransactionFile/readme.txt
@@ -0,0 +1,6 @@
+使用KTM(内核事务管理器)进行文件事务处理
+http://www.cnblogs.com/wangiqngpei557/archive/2012/01/12/2320965.html
+
+
+http://download.microsoft.com/download/f/2/7/f279e71e-efb0-4155-873d-5554a0608523/TxF2007_07.exe
+
